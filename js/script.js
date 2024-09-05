6e32a421f22031bc6c2dde497c45a31ab0d068e3
Marcap00 <cappellatomarco@yahoo.com>
2024-09-05 15:42:05 +0200

    created array of objects todo items

--- js/script.js
+++ js/script.js
@@ -3,7 +3,16 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: 'Hello Vue!',
+            todoItems: [
+                { title: 'Completare esercizi JS', done: false },
+                { title: 'Completare esercizi Vue', done: true },
+                { title: 'Leggere 100 pagine di un libro', done: true },
+                { title: 'Comprare nuove cuffie', done: true },
+                { title: 'Leggere 5 pagine di documentazione', done: false },
+                { title: "Ripassare argomenti dell'ultima lezione", done: false },
+                { title: 'Portare a fare una passeggiata il cane', done: true },
+                { title: 'Aggiungere una pagina alle note personali', done: false }
+            ]
         }
     }
 }).mount('#app');
