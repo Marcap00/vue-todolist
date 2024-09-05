fe89e92bf23d0a0410e91b8dd27476aec359db72
Marcap00 <cappellatomarco@yahoo.com>
2024-09-05 17:59:36 +0200

    added input and button to add new items to the todo list

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+/* 
+MILESTONE 1
+Stampare all'interno di una lista HTML un item per ogni todo.
+Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+
+MILESTONE 2
+Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+
+MILESTONE 3
+Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+
+Bonus:
+1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa) */
+
+
 const { createApp } = Vue;
 
 createApp({
@@ -13,13 +29,25 @@ createApp({
                 { title: "Ripassare argomenti dell'ultima lezione", done: false },
                 { title: 'Portare a fare una passeggiata il cane', done: true },
                 { title: 'Aggiungere una pagina alle note personali', done: false }
-            ]
+            ],
+            // Inizializzo l'elementto che voglio aggiungere alla lista
+            newTodoItem: "",
         }
     },
     methods: {
         // Metodo per rimuovere un elemento dalla to do list (ricevendo come parametro l'indice dell'elemento)
         removeItem(i) {
+            // Rimuovo l'elemento dalla lista utilizzando il metodo splice
             this.todoItems.splice(i, 1);
+        },
+        // Metodo per aggiungere un elemento alla to do list
+        addElement() {
+            // Controllo che il campo di input non sia vuoto
+            if (!this.newTodoItem) return;
+            // Aggiungo l'elemento alla to do list
+            this.todoItems.push({ title: this.newTodoItem, done: false });
+            // Resetto il campo di input
+            this.newTodoItem = '';
         }
     },
 }).mount('#app');
